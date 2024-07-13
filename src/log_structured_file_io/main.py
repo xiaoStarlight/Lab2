b38766266137669427b47e6a1b9190a5fe8281df
Xiao Starlight <153096641+xiaoStarlight@users.noreply.github.com>
2024-07-13 15:49:57 +0800

    Update main.py

--- src/log_structured_file_io/main.py
+++ src/log_structured_file_io/main.py
@@ -1,8 +1,44 @@
+# 文件名: src/log_structured_file_io/main.py
 import argparse
-
+import json
 def main():
     arg_parser = argparse.ArgumentParser(description='Log structured file I/O')
-    ...
-    
+    subparsers = arg_parser.add_subparsers(dest='command')
+
+        # Set command
+    arg_parser_set = subparsers.add_parser('set', help='Set a key-value pair')
+    arg_parser_set.add_argument('key', type=str, help='Key')
+    arg_parser_set.add_argument('value', type=str, help='Value')
+
+        # Get command
+    arg_parser_get = subparsers.add_parser('get', help='Get the value of a key')
+    arg_parser_get.add_argument('key', type=str, help='Key')
+
+        # Del command
+    arg_parser_del = subparsers.add_parser('del', help='Delete a key-value pair')
+    arg_parser_del.add_argument('key', type=str, help='Key')
+
+    args = arg_parser.parse_args()
+    with open('./data/store.kv', 'r') as f:
+        data = json.load(f)
+    if args.command == 'set':
+        data[args.key] = args.value
+        print(f'Key "{args.key}" set to "{args.value}"')
+    elif args.command == 'get':
+        value = data.get(args.key)
+        if value is None:
+            print('Key not found')
+        else:
+            print(value)
+    elif args.command == 'del':
+        if args.key in data:
+            del data[args.key]
+            print(f'Key "{args.key}" deleted')
+        else:
+            print('Key not found')
+    with open('./data/store.kv', "w") as f:
+        json.dump(data, f)
+
+
 if __name__ == '__main__':
     main()
